fa67f5d59b672dd125a1d3d6cf33c813b956e772
SirPm <odumosuoluwapm@gmail.com>
2025-05-20 04:15:47 +0100

    - Waitlist Init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+// import viteLogo from "/vite.svg";
+
+{
+	/* <img src={viteLogo} className="logo" alt="Vite logo" /> */
+}
+
+function App() {
+	return <>WELCOME TO NECTAR</>;
+}
+
+export default App;
